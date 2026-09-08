2ac572d1a1109b007e46b9bb2aeaaadca5ec5b19
VixToryH bc2025-3 <Viktoriia.Hvozdevych@lnu.edu.ua>
2026-09-08 21:01:15 +0300

    lab01: оновлено предметну область Бібліотека

--- src/Cli/Program.cs
+++ src/Cli/Program.cs
@@ -1,6 +1,14 @@
-﻿using System;
+﻿using System.Runtime.InteropServices;
 
-Console.WriteLine("Вітаю!");
-Console.WriteLine("Проєкт: MusicRent");
-Console.WriteLine($"ОС: {Environment.OSVersion}");
-Console.WriteLine($"Версія .NET: {Environment.Version}");
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Гвоздевич Вікторія, група ФЕІ-36");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"ОС (OSDescription) : {RuntimeInformation.OSDescription}");
+Console.WriteLine($"ОС (Environment) : {Environment.OSVersion}");
+Console.WriteLine($"Архітектура процесу : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR) : {Environment.Version}");
+Console.WriteLine($"Runtime : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: Бібліотека — облік видач примірників книг читачам.");
