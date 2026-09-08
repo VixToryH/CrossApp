6e400366c145dc6f37d1e500b30238f9c259248e
VixToryH bc2025-3 <Viktoriia.Hvozdevych@lnu.edu.ua>
2026-09-08 20:31:07 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("Вітаю!");
+Console.WriteLine("Проєкт: MusicRent");
+Console.WriteLine($"ОС: {Environment.OSVersion}");
+Console.WriteLine($"Версія .NET: {Environment.Version}");
